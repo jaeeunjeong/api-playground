70d71af1c1c910070b737ef4a10e6a7f13adc577
jaeeunjeong <jejeong000@gmail.com>
2023-06-11 15:18:16 +0900

    add : 사용자 가입/ 정보 조회/ 탈퇴 프로세스 구현

--- src/main/java/com/jejeong/apipractice/sevice/member/GeneralMemberSignService.java
+++ src/main/java/com/jejeong/apipractice/sevice/member/GeneralMemberSignService.java
@@ -1,16 +1,18 @@
 package com.jejeong.apipractice.sevice.member;
 
-import com.jejeong.apipractice.controller.member.request.SignUpRequest;
+import com.jejeong.apipractice.controller.sign.request.SignUpRequest;
 import com.jejeong.apipractice.dto.member.MemberDto;
 import com.jejeong.apipractice.entity.member.Member;
 import com.jejeong.apipractice.repository.member.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class GeneralMemberService implements MemberService {
+public class GeneralMemberSignService implements SignService {
 
     private final MemberRepository memberRepository;
 
@@ -19,18 +21,19 @@ public class GeneralMemberService implements MemberService {
     public void signUp(SignUpRequest req) {
         validateSignUpInfo(req);
         // TODO password encoding
-        memberRepository.saveAndFlush(Member.of(req.getEmail(), req.getPassword(), req.getNickname()));
+        Member member = Member.of(req.getEmail(), req.getPassword(), req.getNickname());
+        memberRepository.saveAndFlush(member);
     }
 
     @Override
     public void validateSignUpInfo(SignUpRequest req) {
-        if (memberRepository.existsByEmail(req.getEmail())) throw new RuntimeException();
-        if (memberRepository.existsByNickname(req.getNickname())) throw new RuntimeException();
+        if (memberRepository.existsByEmail(req.getEmail())) throw new IllegalArgumentException();
+        if (memberRepository.existsByNickname(req.getNickname())) throw new IllegalArgumentException();
     }
 
     @Override
     public MemberDto loadUserByUserEmail(String email) {
-        return memberRepository.findByEmail(email).map(MemberDto::fromEntity).orElseThrow(RuntimeException::new);
+        return memberRepository.findByEmail(email).map(MemberDto::fromEntity).orElseThrow(IllegalArgumentException::new);
     }
 
     @Override
@@ -38,6 +41,5 @@ public class GeneralMemberService implements MemberService {
     public void deleteUserByUserEmail(String email) {
         Member member = memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
         member.hideWithdrawalInfo();
-        memberRepository.delete(member);
     }
 }
